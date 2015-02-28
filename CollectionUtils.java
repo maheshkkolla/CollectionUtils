b4338bef907501d173c3ef17f50c11b6c99cd947
unknown <maheshkk@INMAHESKK.corporate.thoughtworks.com>
2015-02-28 12:52:17 +0530

    filter is complted, ListFilter interface is created

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -1,19 +1,27 @@
 import java.util.*;
 
 interface ListMapper<T> {
-	public T callback(T item);
+	public T callbackForMap(T item);
+}
+
+interface ListFilter<T> {
+	public Boolean callbackForFilter(T item);
 }
 
 public	class CollectionUtils{
 	public static <E> List<E> map(List<E> myList, ListMapper<E> callbackObj) {
 		List<E> resultList = new ArrayList<E>();
 		for( E item: myList){
-			resultList.add(callbackObj.callback(item));
+			resultList.add(callbackObj.callbackForMap(item));
 		}
 		return resultList;
 	}
 
-	// public static List<E> filter(List<E>, ListFilter ){
-
-	// }
+	public static <E> List<E> filter(List<E> myList, ListFilter<E> callbackObj){
+		List<E> resultList = new ArrayList<E>();
+		for( E item: myList){
+			if(callbackObj.callbackForFilter(item)) resultList.add(item);
+		}
+		return resultList;
+	}
 }
--- CollectionUtilsTest.java
+++ CollectionUtilsTest.java
@@ -5,21 +5,34 @@ import java.util.*;
 public class CollectionUtilsTest {
 	@Test
 	public void map_should_iterate_throught_the_list_and_gives_the_result_list() {
-		ListMapper<Integer> callbackObj = new mapCallback();
+		ListMapper<Integer> callbackObj = new Callback();
 		List<Integer> myList = new ArrayList<Integer>();
 		myList.add(1);
 		myList.add(2);
 		myList.add(3); 
 		List<Integer> resultList = CollectionUtils.<Integer>map(myList,callbackObj);
-		assertEquals(resultList.get(0),(Integer)1);
-		assertEquals(resultList.get(1),(Integer)1);
-		assertEquals(resultList.get(2),(Integer)1);
+		assertEquals(resultList.get(0),(Integer)2);
+		assertEquals(resultList.get(1),(Integer)3);
+		assertEquals(resultList.get(2),(Integer)4);
+	}
+	@Test
+	public void filter_should_return_a_list_with_filtered_items_given_as_per_condition_in_callback() {
+		ListFilter<Integer> callbackObj = new Callback();
+		List<Integer> myList = new ArrayList<Integer>();
+		myList.add(1);
+		myList.add(2);
+		myList.add(3); 
+		List<Integer> resultList = CollectionUtils.<Integer>filter(myList,callbackObj);
+		assertEquals(resultList.get(0),(Integer)2);
 	}
 }
 
-class mapCallback implements ListMapper<Integer>{
-	public Integer callback(Integer item) {
-		return (Integer)1;
+class Callback implements ListMapper<Integer>, ListFilter<Integer> {
+	public Integer callbackForMap(Integer item) {
+		return (Integer)(item + 1);
+	}
+	public Boolean callbackForFilter(Integer item) {
+		return (item % 2 == 0); 
 	}
 }
 
